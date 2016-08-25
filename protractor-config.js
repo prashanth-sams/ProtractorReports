27f3cbfaea416996ec67cd155482d567b51ebd9d
Prashanth Sams <sams.pickoh@gmail.com>
2016-08-25 14:06:37 +0530

    allure

--- protractor-config.js
+++ protractor-config.js
@@ -39,11 +39,17 @@ exports.config = {
     defaultTimeoutInterval: 300000
   },
 
-  onPrepare: function() {
+  onPrepare: function () {
     var AllureReporter = require('jasmine-allure-reporter');
-    jasmine.getEnv().addReporter(new AllureReporter({
-      resultsDir: 'allure-results'
-    }));
+    jasmine.getEnv().addReporter(new AllureReporter());
+    jasmine.getEnv().afterEach(function(done){
+      browser.takeScreenshot().then(function (png) {
+        allure.createAttachment('Screenshot', function () {
+          return new Buffer(png, 'base64');
+        }, 'image/png')();
+        done();
+      });
+    });
   },
 
   allScriptsTimeout: 200000
